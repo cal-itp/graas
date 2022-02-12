c030a6550d25192b10054277a757246cb7a87cc4
Scott Owades <jeremyscottowades@gmail.com>
2022-02-11 16:22:30 -0800

    load maps

--- gtfu/src/web/graas-report-web.js
+++ gtfu/src/web/graas-report-web.js
@@ -1,4 +1,6 @@
 var scaleRatio;
+const maps = new Map()
+const timelines = new Map()
 
 function draw(){
     console.log("drawing!");
@@ -18,12 +20,24 @@ function draw(){
 
 var obj;
 
-fetch("https://storage.googleapis.com/graas-resources/web/graas-report-web-5.json")
+fetch("https://storage.googleapis.com/graas-resources/web/graas-report-web-6.json")
 .then(response => {
    return response.json();
 })
 .then(jsondata => loadObject(jsondata))
 
 function loadObject(object){
-    console.log(object.trips[0].boundaries);
+    for (var i = 0; i < object.trips.length; i++) {
+        timelines.set(object.trips[i]["trip-id"], {timeline_start_x: object["trips"][i]["boundaries"]["timeline-start-x"],
+                                                    timeline_start_y: object["trips"][i]["boundaries"]["timeline-start-y"],
+                                                    timeline_end_x: object["trips"][i]["boundaries"]["timeline-end-x"],
+                                                    timeline_end_y: object["trips"][i]["boundaries"]["timeline-end-y"]
+                                                    })
+
+        maps.set(object.trips[i]["trip-id"], {map_start_x: object["trips"][i]["boundaries"]["map-start-x"],
+                                                map_start_y: object["trips"][i]["boundaries"]["map-start-y"],
+                                                map_end_x: object["trips"][i]["boundaries"]["map-end-x"],
+                                                map_end_y: object["trips"][i]["boundaries"]["map-end-y"]
+                                                })
+    }
 }
