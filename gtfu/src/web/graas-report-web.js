6c3b3439c5c3de233f6239ac07887be82b95058c
Scott Owades <jeremyscottowades@gmail.com>
2022-02-17 18:57:39 -0800

    refactor

--- gtfu/src/web/graas-report-web.js
+++ gtfu/src/web/graas-report-web.js
@@ -1,31 +1,39 @@
-var scaleRatio;
+
 const trips = new Map();
 const agencies = new Map();
-var mapCanvas
-var mapCtx;
-var timelineCanvas;
-var timelineCtx
-const img = new Image();
-var canvasWidth;
-var windowHeight;
-var timelineHeight;
-var imageWidth;
-var imageHeight;
-var timelineCanvasHeight;
-var mapCanvasHeight;
+
 var selectedAgency;
 var selectedDate;
+
+const img = new Image();
+var imageWidth;
+var imageHeight;
+
+var scaleRatio;
+var canvasWidth;
+
+var mapCanvas
+var mapCtx;
+var mapHeight;
+var mapCanvasHeight;
+
+var timelineCanvas;
+var timelineCtx
+var timelineHeight;
+var timelineCanvasHeight;
+
 var dropdownHeight;
 var headerHeight;
+var windowHeight;
 var mapWindowHeight
+
 var scrollTop;
 var timelineScrollTop;
 var mapScrollTop;
-var mapScrollBottom;
+var scrollBottom;
 
 var font_size = 20;
-const bucketURL = "https://storage.googleapis.com/graas-resources/"
-const MIN_HEIGHT = 1000;
+const bucketURL = "https://storage.googleapis.com/graas-resources"
 const font = "Arial";
 const tooltipItems = ["trip_id", "vehicle_id", "agent", "device",
                      "os", "uuid_tail", "avg_update_interval",
@@ -46,12 +54,11 @@ function load(){
 
     timelineCanvas = document.getElementById('timeline');
     timelineCtx = timelineCanvas.getContext('2d');
-    timelineCtx.canvas.width  = canvasWidth;
+    timelineCtx.canvas.width = canvasWidth;
 
-    var dropdowns = document.getElementById('dropdowns');
-    dropdownHeight = dropdowns.offsetHeight;
+    dropdownHeight = document.getElementById('dropdowns').offsetHeight;
 
-    img.src = bucketURL + "graas-report-archive/" + selectedAgency + "/" + selectedAgency + "-" + selectedDate + "-dev.png?nocache=123";
+    img.src = `${bucketURL}/graas-report-archive/${selectedAgency}/${selectedAgency}-${selectedDate}-dev.png`;
 
     img.onload = function() {
         imageWidth = this.naturalWidth
@@ -65,75 +72,79 @@ function load(){
         headerHeight = timelineCanvasHeight + dropdownHeight
         timelineCtx.canvas.height = timelineCanvasHeight;
 
-        mapCanvasHeight = (imageHeight - timelineHeight) * scaleRatio;
+        mapHeight = imageHeight - timelineHeight;
+        mapCanvasHeight = mapHeight * scaleRatio;
         mapCtx.canvas.height = mapCanvasHeight;
         mapWindowHeight = windowHeight - headerHeight;
+
+        // It would be nice to do this while loading JSON,
+        // but that occurs before image is loaded and scaleRatio is determined
+        for (const [key, value] of trips.entries()) {
+            value.map.x *= scaleRatio;
+            value.map.y *= scaleRatio;
+            value.map.width *= scaleRatio;
+            value.map.height *= scaleRatio;
+            value.timeline.x *= scaleRatio;
+            value.timeline.y *= scaleRatio;
+            value.timeline.width *= scaleRatio;
+            value.timeline.height *= scaleRatio;
+        }
 
         drawReport();
     };
 
-    // Add event listener for `click` events.
     document.body.addEventListener('click', function(event) {
-        console.log(event);
-        var x = event.pageX;
-        var y = event.pageY;
-        // console.log("x:" + x);
-        // console.log("y:" + y);
+        console.log("event.pageY:" + event.pageY);
 
+        drawReport();
+
+        // Top of current screen
         scrollTop = document.documentElement.scrollTop;
+        // Top of current timeline
         timelineScrollTop = scrollTop + dropdownHeight;
+        // Top of current map
         mapScrollTop = scrollTop + headerHeight;
-        mapScrollBottom = mapScrollTop + mapWindowHeight;
+        // Bottom of current map
+        scrollBottom = scrollTop + windowHeight;
 
-        var foundTrip = false;
-        if(y >= (mapScrollTop)){
-            for (const [key, value] of trips.entries()) {
-                if (mapContainsPoint(value, x, y - headerHeight)){
-                    selectTrip(value);
-                    drawMetadata(value);
-                    foundTrip = true;
-                    break;
-                }
+        for (const [key, value] of trips.entries()) {
+            var searchY = event.pageY;
+            var searchObject;
+
+            // If the click occurs below the header, adjust y value and search map objects
+            if(searchY >= mapScrollTop){
+                searchY -=headerHeight;
+                searchObject = value.map;
+
+            // If the click occurs within the header, adjust y value and search timeline objects
+            } else {
+                searchY -=timelineScrollTop;
+                searchObject = value.timeline;
             }
-        } else {
-            for (const [key, value] of trips.entries()) {
-                if (timelineContainsPoint(value, x, y - timelineScrollTop)){
-                    selectTrip(value);
-                    drawMetadata(value);
-                    foundTrip = true;
-                    break;
-                }
+            if (objectContainsPoint(searchObject, event.pageX, searchY)){
+                drawMetadata(value);
+                selectTrip(mapCtx, value.map);
+                selectTrip(timelineCtx, value.timeline);
+                scrollToTrip(value)
+                break;
             }
         }
-        if (!foundTrip){
-            // Since we can't simply remove rectangle, reload the map
-            drawReport()
-        }
-    }, false);
+    });
 }
 
 function drawReport(){
     console.log("Drawing map...");
     timelineCtx.drawImage(img, 0, 0, imageWidth, timelineHeight, 0, 0, canvasWidth, timelineCanvasHeight);
-    mapCtx.drawImage(img, 0, timelineHeight, imageWidth, imageHeight - timelineHeight, 0, 0, canvasWidth, mapCanvasHeight);
+    mapCtx.drawImage(img, 0, timelineHeight, imageWidth, mapHeight, 0, 0, canvasWidth, mapCanvasHeight);
 }
 
-function loadDropdownJSON(){
+function loadJSON(url, callback){
     // ?nocache= prevents annoying json caching...mostly for debugging purposes
-    fetch(bucketURL + "web/graas-report-agency-dates.json?nocache="  + (new Date()).getTime())
+    fetch(url + "?nocache="  + (new Date()).getTime())
     .then(response => {
        return response.json();
     })
-    .then(jsondata => processDropdownJSON(jsondata))
-}
-
-function loadTripJSON(){
-    // ?nocache= prevents annoying json caching...mostly for debugging purposes
-    fetch(bucketURL + "graas-report-archive/" + selectedAgency + "/" + selectedAgency + "-" + selectedDate + ".json?nocache=" + (new Date()).getTime())
-    .then(response => {
-       return response.json();
-    })
-    .then(jsondata => processTripJSON(jsondata))
+    .then(jsondata => callback(jsondata))
 }
 
 function processDropdownJSON(object){
@@ -161,6 +172,7 @@ function handleAgencyChoice(){
     var p = document.getElementById("date-select");
     clearSelectOptions(p);
 
+    // loop counts down to get reverse-chronological dates
     for (var i = agencies.get(selectedAgency).length -1; i >= 0; i--) {
         var opt = document.createElement('option');
         opt.appendChild(document.createTextNode(agencies.get(selectedAgency)[i]));
@@ -172,11 +184,24 @@ function handleAgencyChoice(){
 function handleDateChoice(){
     console.log("handleDateChoice()");
     selectedDate = document.getElementById("date-select").value;
-    loadTripJSON();
+    url = `${bucketURL}/graas-report-archive/${selectedAgency}/${selectedAgency}-${selectedDate}.json`;
+    loadJSON(url, processTripJSON);
     load();
 }
+
 function processTripJSON(object){
     for (var i = 0; i < object.trips.length; i++) {
+        var map = {x: object["trips"][i]["boundaries"]["map-x"],
+                   y: object["trips"][i]["boundaries"]["map-y"],
+                   width: object["trips"][i]["boundaries"]["map-width"],
+                   height: object["trips"][i]["boundaries"]["map-height"]
+                };
+        var timeline = {x: object["trips"][i]["boundaries"]["timeline-x"],
+                       y: object["trips"][i]["boundaries"]["timeline-y"],
+                       width: object["trips"][i]["boundaries"]["timeline-width"],
+                       height: object["trips"][i]["boundaries"]["timeline-height"]
+                };
+
         // Consider switching from map to array
         trips.set(object.trips[i]["trip-id"], {trip_name: object["trips"][i]["trip-name"],
                                                 agent: object["trips"][i]["agent"],
@@ -188,42 +213,34 @@ function processTripJSON(object){
                                                 avg_update_interval: object["trips"][i]["avg-update-interval"],
                                                 min_update_interval: object["trips"][i]["min-update-interval"],
                                                 vehicle_id: object["trips"][i]["vehicle-id"],
-                                                timeline_x: object["trips"][i]["boundaries"]["timeline-x"],
-                                                timeline_y: object["trips"][i]["boundaries"]["timeline-y"],
-                                                timeline_width: object["trips"][i]["boundaries"]["timeline-width"],
-                                                timeline_height: object["trips"][i]["boundaries"]["timeline-height"],
-                                                map_x: object["trips"][i]["boundaries"]["map-x"],
-                                                map_y: object["trips"][i]["boundaries"]["map-y"],
-                                                map_width: object["trips"][i]["boundaries"]["map-width"],
-                                                map_height: object["trips"][i]["boundaries"]["map-height"],
+                                                map: map,
+                                                timeline: timeline
                                                 })
     }
     timelineHeight = object["header-height"];
 }
 
-function selectTrip(trip){
-    console.log("selectTrip()");
-    drawReport();
-    mapCtx.beginPath();
-    mapCtx.lineWidth = 4;
-    mapCtx.strokeStyle = "green";
+function selectTrip(ctx, object){
+    ctx.beginPath();
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "green";
+    ctx.rect(object.x, object.y, object.width, object.height);
+    ctx.stroke();
+}
 
-    var mapY = trip.map_y * scaleRatio;
-    var mapHeight = trip.map_height * scaleRatio;
-    mapCtx.rect(trip.map_x * scaleRatio, mapY, trip.map_width * scaleRatio, trip.map_height * scaleRatio);
-    mapCtx.stroke();
+// Currently not working when lower half of trip overhangs bottom of screen
+function scrollToTrip(trip){
+    var mapY = trip.map.y;
+    var tripMapHeight = trip.map.height;
 
+    console.log(`scrollBottom: ${scrollBottom}`)
+    console.log(`mapY + tripMapHeight: ${mapY + tripMapHeight}`)
+    console.log(`mapY: ${mapY}`)
+    console.log(`tripMapHeight: ${tripMapHeight}`)
     // Center the mapview if it's overlapping an edge
-    if(mapY < mapScrollTop || mapY + mapHeight > mapScrollBottom){
-        scrollTop = mapY  + mapHeight * 1/3 - headerHeight ;
-        document.documentElement.scrollTop = scrollTop;
+    if(mapY < mapScrollTop || mapY + tripMapHeight > scrollBottom){
+        document.documentElement.scrollTop = mapY  + tripMapHeight * 1/3 - headerHeight;
     }
-
-    timelineCtx.beginPath();
-    timelineCtx.lineWidth = 4;
-    timelineCtx.strokeStyle = "green";
-    timelineCtx.rect(trip.timeline_x * scaleRatio, trip.timeline_y * scaleRatio, trip.timeline_width * scaleRatio, trip.timeline_height * scaleRatio);
-    timelineCtx.stroke();
 }
 
 function drawMetadata(trip){
@@ -231,20 +248,20 @@ function drawMetadata(trip){
 
     var margin = 5;
 
-    mapWidth = trip["map_width"] * scaleRatio;
-    metadataX = trip["map_x"] * scaleRatio + mapWidth;
-    metadataY = trip["map_y"] * scaleRatio;
-    mapHeight = trip ["map_height"] * scaleRatio;
+    metadataWidth = trip.map.width;
+    metadataX = trip.map.x + metadataWidth;
+    metadataY = trip.map.y;
+    metadataHeight = trip.map.height;
 
-    while ((tooltipItems.length * (font_size + margin) >= mapHeight)
-            || (getMaxTextWidth(trip) + margin * 2 >= mapWidth))  {
+    while ((tooltipItems.length * (font_size + margin) >= metadataHeight)
+            || (getMaxTextWidth(trip) + margin * 2 >= metadataWidth))  {
         console.log("reducing font size...");
         font_size -= 5;
     }
 
     // Metadata on right side unless it would go over edge
-    if((metadataX + mapWidth) > canvasWidth){
-        metadataX -= (2 * mapWidth);
+    if((metadataX + metadataWidth) > canvasWidth){
+        metadataX -= (2 * metadataWidth);
     }
 
     // Draw tooltip box outline
@@ -252,17 +269,17 @@ function drawMetadata(trip){
     mapCtx.lineWidth = 1
     mapCtx.strokeStyle = "black";
     mapCtx.fillStyle = "white";
-    mapCtx.fillRect(metadataX, metadataY, mapWidth, mapHeight);
-    mapCtx.rect(metadataX, metadataY, mapWidth, mapHeight);
+    mapCtx.fillRect(metadataX, metadataY, metadataWidth, metadataHeight);
+    mapCtx.rect(metadataX, metadataY, metadataWidth, metadataHeight);
 
     // Draw tooltip text
     mapCtx.stroke();
     mapCtx.fillStyle = "black";
-    mapCtx.font= font_size + "px " + font;
+    mapCtx.font= `${font_size}px ${font}`;
 
     for (var i = 0; i < tooltipItems.length; i++) {
         metadataY += (font_size + margin);
-        mapCtx.fillText(tooltipItems[i] + ": " + trip[tooltipItems[i]], metadataX + margin , metadataY);
+        mapCtx.fillText(`${tooltipItems[i]}: ${trip[tooltipItems[i]]}`, metadataX + margin , metadataY);
     }
 }
 
@@ -279,30 +296,17 @@ function getMaxTextWidth(trip) {
     return Math.round(maxWidth);
 }
 
-function mapContainsPoint(object, x, y){
-    if(x >= object.map_x * scaleRatio){
-        if(x <= (object.map_x + object.map_width) * scaleRatio){
-            if(y >= object.map_y * scaleRatio ){
-                if(y <= (object.map_y + object.map_height) * scaleRatio){
+function objectContainsPoint(object, x, y){
+    if(x >= object.x){
+        if(x <= (object.x + object.width)){
+            if(y >= object.y ){
+                if(y <= (object.y + object.height)){
                     return true;
                 }
             }
         }
     }
     return false
-}
-
-function timelineContainsPoint(object, x, y){
-    if(x >= object.timeline_x * scaleRatio){
-        if(x <= (object.timeline_x + object.timeline_width) * scaleRatio){
-            if(y >= object.timeline_y * scaleRatio){
-                if(y <= (object.timeline_y + object.timeline_height) * scaleRatio){
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
 }
 
 function clearSelectOptions(sel) {
@@ -313,4 +317,5 @@ function clearSelectOptions(sel) {
     }
 }
 
-loadDropdownJSON();
+url = `${bucketURL}/web/graas-report-agency-dates.json`;
+loadJSON(url, processDropdownJSON);
