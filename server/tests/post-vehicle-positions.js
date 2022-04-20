00d37eddc4457f84034ac685066dfa274a62db75
wildcard <kay.neuenhofen@gmail.com>
2022-04-20 13:14:56 -0700

    full roundtrip test for vehicle position feed.

--- server/tests/post-vehicle-positions.js
+++ server/tests/post-vehicle-positions.js
@@ -1,7 +1,33 @@
+/*
+A roundtrip test of server vehicle position feed functionality.
+
+First, we post a batch of vehicle position updates. Count and vehicle IDs
+are determinded by the contents of `VEHICLE_IDS`. The wrinkle here is that
+some vehicle IDs are duplicate and the server will only keep the most recent
+position update for a vehicle for the purposes of the vehicle position feed.
+
+Then, we download the current vehicle position feed and iterate over the
+contained vehicle entries, checking that timestamps are current and that
+vehicle IDs start with `pr-test-vehicle-id-` (this is useful to e.g. weed
+out updates sent as part of different tests).
+
+Finally, we compare a sorted list of unique entries in `VEHICLE_IDS` with
+the sorted list of current vehicle IDs received in the feed. If the lists
+aren't identical, the test fails.
+*/
+
 const crypto = require('crypto').webcrypto
 const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const util = require('../app-engine/static/gtfs-rt-util');
 const fetch = require('node-fetch');
+
+const VEHICLE_IDS = [
+    'pr-test-vehicle-id-1', 'pr-test-vehicle-id-2',
+    'pr-test-vehicle-id-2', 'pr-test-vehicle-id-3',
+    'pr-test-vehicle-id-4', 'pr-test-vehicle-id-5',
+    'pr-test-vehicle-id-5', 'pr-test-vehicle-id-5',
+    'pr-test-vehicle-id-6', 'pr-test-vehicle-id-7'
+];
 
 function getBaseName(s) {
     const index = s.lastIndexOf('/');
@@ -18,10 +44,10 @@ async function getResponseBody(url) {
     const response = await fetch(url, requestSettings);
 
     const blob = await response.blob();
-    util.log(`- blob: ${blob}`);
+    //util.log(`- blob: ${blob}`);
 
     const arrayBuf = await blob.arrayBuffer();
-    util.log(`- arrayBuf.byteLength: ${arrayBuf.byteLength}`);
+    //util.log(`- arrayBuf.byteLength: ${arrayBuf.byteLength}`);
 
     const body = Buffer.from(arrayBuf);
     //util.log(`- body: ${JSON.stringify(body)}`);
@@ -32,7 +58,7 @@ async function getResponseBody(url) {
 async function getSignatureKey() {
     const base64 = process.env.PR_TEST_ID_ECDSA;
 
-    if (!base64) throw 'no private key found in $PR_TEST_ID_ECDSA';
+    if (!base64) throw 'unset environment variable $PR_TEST_ID_ECDSA';
 
     const key = atob(base64);
     util.log("- key.length: " + key.length);
@@ -51,6 +77,49 @@ async function getSignatureKey() {
     );
 }
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+function getEpochSeconds() {
+    return Math.floor(Date.now() / 1000);
+}
+
+async function postUpdates(signatureKey, url) {
+    let data = {
+        uuid: 'test',
+        agent: 'node',
+        timestamp: 0,
+        lat: 0,
+        long: 0,
+        speed: 0,
+        heading: 0,
+        accuracy: 0,
+        version: 'test'
+    };
+
+    data['trip-id'] = 'test';
+    data['agency-id'] = 'test';
+    data['vehicle-id'] = 'test';
+    data['pos-timestamp'] = 'test';
+
+    util.log('- signatureKey: ' + signatureKey);
+    util.log('- data: ' + JSON.stringify(data));
+
+    for (let vid of VEHICLE_IDS) {
+        data['vehicle-id'] = vid;
+        util.log(`-- vid: ${vid}`);
+
+        data['timestamp'] = getEpochSeconds();
+        util.log(`++ timestamp: ${data.timestamp}`);
+
+        util.signAndPost(data, signatureKey, url);
+        await sleep(1000);
+    }
+}
+
 async function test(url) {
     util.log(`test()`);
     util.log(`- url: ${url}`);
@@ -60,16 +129,75 @@ async function test(url) {
     const signatureKey = await getSignatureKey();
     util.log(`- signatureKey: ${JSON.stringify(signatureKey)}`);
 
-    const body = await getResponseBody(args[1]);
-    util.log(`- body: ${JSON.stringify(body)}`);
+    await postUpdates(signatureKey, url + '/new-pos-sig');
 
+    console.log('sleeping for a few seconds before accessing vehicle position feed...');
+
+    for (let i=0; i<5; i++) {
+        await sleep(1000);
+        console.log('.');
+    }
+
+    console.log('done');
+
+    const now = getEpochSeconds();
+    const body = await getResponseBody(url + '/vehicle-positions.pb?agency=test');
     const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
 
+    let expectedVIDS = [];
+
+    for (let vid of VEHICLE_IDS) {
+        if (!expectedVIDS.includes(vid)) {
+            expectedVIDS.push(vid);
+        }
+    }
+
+    expectedVIDS.sort();
+    const expected = JSON.stringify(expectedVIDS);
+    util.log(`- expected: ${expected}`);
+
+    let currentVIDS = [];
+
     feed.entity.forEach(function(entity) {
-        //if (entity.trip_update) {
-            util.log(entity);
-        //}
+        if (entity.vehicle) {
+            let current = false;
+
+            if (entity.vehicle.timestamp && entity.vehicle.timestamp.low) {
+                //util.log(`++ timestamp: ${entity.vehicle.timestamp.low}`);
+
+                const timestamp = entity.vehicle.timestamp.low;
+                const delta = Math.abs(now - timestamp);
+                //util.log(`++ delta: ${delta}`);
+
+                if (delta < 60) {
+                    current = true;
+                } else {
+                    util.log(`** excessive delta: ${delta}, discarding entity ${entity.vehicle}`);
+                }
+            }
+
+            if (entity.vehicle.vehicle && entity.vehicle.vehicle.label) {
+                //util.log(`++ vehicle ID: ${entity.vehicle.vehicle.label}`);
+
+                const vid = entity.vehicle.vehicle.label;
+
+                if (current && vid.indexOf('pr-test-vehicle-id-') === 0) {
+                    currentVIDS.push(vid);
+                }
+            }
+        }
     });
+
+    currentVIDS.sort();
+
+    const current = JSON.stringify(currentVIDS);
+    util.log(`- current : ${current}`);
+
+    if (expected === current) {
+        util.log('---> test succeeded');
+    } else {
+        throw `expected '${expected}' but got '${current}'`;
+    }
 }
 
 const args = process.argv.slice(1);
