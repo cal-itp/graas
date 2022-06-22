d1dd8bf70663bb100b3cf1cd64b1d012da285746
wildcard <kay.neuenhofen@gmail.com>
2022-06-21 18:28:10 -0700

    flesh out db sim more, still incomplete and untested

--- server/app-engine/db.py
+++ server/app-engine/db.py
@@ -1,35 +1,23 @@
 """
-datastore interface:
-Client():
-- query(kind='<kind>') # returns Query instance
-- get(<key>)
-- put(<entity>)
-- put_multi(<list of entities>)
-- delete(<key>)
-- delete_multi(<list of keys>)
-- key('alert')) # datastore.Key
-- Entity(key=<key>)
+IMPORTANT NOTE: this a terrible DB simulation meant for only one thing - to let developers get
+a first impression of the system without having to set up an actual database. The implementation
+is naive, inefficient, incomplete and may well lose all data you add to it. You have been warned.
 
-Query():
-- fetch(limit=<n>) # returns Iterator
-- add_filter('time_stop', '<', seconds) # property, operand, value
-- order # property: list of order attributes, '+' for ascending by default, or '-' for descending
-
-
+to do:
 - util.py
   + replace 'from google.cloud import datastore' with 'import from db import db'
   + 'datastore_client = datastore.Client()' -> 'datastore_client = db.Client()'
-  + query = datastore_client.query(kind="agency")
-    results = list(query.fetch())
 """
 import os
 import random
 import json
+import shutil
 import sys
 
 _DB_CLOUD_MODE = 'cloud'
 _DB_LOCAL_MODE = 'local'
-_DB_FILE = 'db-sim.json'
+_DB_FILE     = 'db-sim.json'
+_DB_FILE_BAK = 'db-sim.json.bak'
 
 _db_mode = _DB_CLOUD_MODE
 
@@ -51,68 +39,109 @@ class Client:
                         line = line.rstrip()
                         print(f'-- line: {line}')
                         e = Entity(line)
-                        self.entities[e['key']] = e
+                        self.entities[e['id']] = e
 
-    def query(kind):
+    def query(self, kind):
         if _db_mode == _DB_CLOUD_MODE:
             return self._cloud_client.query(kind)
         else:
             return Query(kind, self)
 
-    def get(key):
+    def get(self, key):
         if _db_mode == _DB_CLOUD_MODE:
             return self._cloud_client.get(key)
         else:
             return self.entities.get(str(key), None)
 
-    def put(entity):
+    def _write(self):
+        shutil.copyfile(_DB_FILE, _DB_FILE_BAK)
+        with open(_DB_FILE, 'w') as f:
+            for e in self.entities.values():
+                e.write(f)
+
+    def put(self, entity, flush=True):
         if _db_mode == _DB_CLOUD_MODE:
             return self._cloud_client.put(entity)
         else:
             self.entities[str(entity['key'])] = entity
 
-    def put_multi(entity_list):
+        if flush:
+            self._write()
+
+    def put_multi(self, entity_list):
         if _db_mode == _DB_CLOUD_MODE:
             return self._cloud_client.put_multi(entity_list)
+        else:
+            for e in entity_list:
+                self.put(e, False)
 
-    def delete(key):
+        self._write()
+
+    def delete(self, key, flush=True):
         if _db_mode == _DB_CLOUD_MODE:
             return self._cloud_client.delete(key)
 
-    def delete_multi(key_list):
+        if flush:
+            self._write()
+
+    def delete_multi(self, key_list):
         if _db_mode == _DB_CLOUD_MODE:
             return self._cloud_client.delete_multi(key_list)
+        else:
+            for k in key_list:
+                self.delete(k, False)
 
-    def key(kind):
+        self._write()
+
+    def key(self, kind):
         if _db_mode == _DB_CLOUD_MODE:
             return self._cloud_client.key(kind)
         else:
             return Key(kind)
 
-    def entity(key):
+    def entity(self, key):
         if _db_mode == _DB_CLOUD_MODE:
             return self._cloud_client.entity(key)
         else:
             return Entity(key)
 
 class Entity(dict):
-    def __init__(self, key):
-        self['key'] = str(key)
+    def __init__(self, arg):
+        if isinstance(arg, Key):
+            self['kind'] = arg.kind
+            self['id'] = arg.id
+        elif isinstance(arg, str):
+            obj = json.parse(arg)
+            for key in obj.keys():
+                self[key] = obj[key]
+        else:
+            raise ValueError('valid arg types are Key and str')
 
-    #def __str__ (self):
-    #    return f'[entity: key={self.key} {super.__str__(self)}]'
+    def write(self, f):
+        f.write(json.dumps(self))
 
-    def read(f):
-        pass
+    def __getitem__(self, key):
+        val = dict.__getitem__(self, key)
+        #print('GET', key)
+        return val
 
-    def write(f):
-        pass
+    def __setitem__(self, key, val):
+        #print('SET', key, val)
+        dict.__setitem__(self, key, val)
 
+    def __repr__(self):
+        dictrepr = dict.__repr__(self)
+        return '%s(%s)' % (type(self).__name__, dictrepr)
+
+    def update(self, *args, **kwargs):
+        #print('update', args, kwargs)
+        for k, v in dict(*args, **kwargs).iteritems():
+            self[k] = v
 
 class Key:
     def __init__(self, kind):
         self.kind = kind
-        self.id = hex(random.getrandbits(128))
+        self.id = hex(random.getrandbits(128))[2:-1]
 
     def __str__ (self):
         return f'{self.kind}@{self.id}'
@@ -125,10 +154,10 @@ class Query:
     def __init__(self, kind, client):
         self.kind = kind
         self.client = client
-        self.order = None
+        self.order = []
         self.filters = []
 
-    def fetch(limit):
+    def fetch(self, limit):
         """
         limit: max number of results
         returns Iterator
@@ -140,9 +169,28 @@ class Query:
         -     results.sort(order)
         - return iter(results)
         """
-        pass
 
-    def add_filter(prop, op, v):
+        self.ret = []
+
+        for e in self.client.entities:
+            if len(self.ret) >= limit:
+                break
+
+            matched = True
+
+            for f in self.filters:
+                if not _matches(e, f):
+                    matched = False
+                    break
+
+            if matched:
+                self.ret.append(e)
+
+        # sort by self.order
+
+        return self.ret
+
+    def add_filter(self, prop, op, v):
         """
         e.g. add_filter('time_stop', '<', seconds):
         # property, operand, value
